a0b2f580ffd4a3e9bb4c4f3d668ab2f0df4c3d04
Breno Carneiro de Faria <breno@Brenos-MacBook-Air.local>
2024-05-31 23:33:38 -0400

    bug fixing

--- decoding_images/image_decoder.py
+++ decoding_images/image_decoder.py
@@ -1,12 +1,14 @@
 import base64
 import os
 
+# Has to be ran from main directory
+
 def image_decoder(img_json_array):
     
-    emptyFolder("./output_images")
+    emptyFolder("./decoding_images/output_images")
  
     for i, image in enumerate(img_json_array):
-        with open(f"./output_images/img{i}.png", "wb") as fo:
+        with open(f"./decoding_images/output_images/img{i}.png", "wb") as fo:
             pngImg = base64.b64decode(image)
             fo.write(pngImg)
 
