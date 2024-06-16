2bf8b2177a1e969801ca0b2fc14fd770001551c1
Breno Carneiro de Faria <breno@Brenos-MacBook-Air.local>
2024-06-15 20:18:11 -0400

    confusion matrix generator implement

--- evaluation.py
+++ evaluation.py
@@ -6,30 +6,10 @@ import torch
 from model import BrenoPeterandSydneysSuperCoolConvolutionalNeuralNetworkVeryAccurateAndGood
 from PIL import Image
 import torchvision.transforms as transforms
-
-def generate_confusion_matrix(model, labeled_data):
-
-    confusion_matrix = [
-        [0, 0],
-        [0, 0]
-    ]
-
-    for data in labeled_data:
-        actual_rating = data[1]
-        predicted_rating = model.predict([data[0]])
-        
-        if actual_rating == predicted_rating:
-            if actual_rating == 1:
-                data[0][0] +=1 # true positive
-            else:
-                data[1][1] +=1 # true negative
-        else:
-            if actual_rating == 1:
-                data[0][1] +=1 # false negative
-            else:
-                data [1][0] +=1 # false positive
-
-    return confusion_matrix
+import os
+from dataset import loadData
+import base64
+import io
 
 def generate_micro_metrics_row(confusion_matrix_array):
 
@@ -58,27 +38,88 @@ def generate_metrics_row(confusion_matrix):
     return res
 
 
-# Load the model
-model = BrenoPeterandSydneysSuperCoolConvolutionalNeuralNetworkVeryAccurateAndGood()
-model.load_state_dict(torch.load('model.pth'))
-model.eval()
+def predict_emotion(model, img):
 
-# Define preprocessing transformations (same as in training)
-preprocess = transforms.Compose([
-    transforms.Resize((48, 48)),  # Resize image to match training input size
-    transforms.ToTensor(),  # Convert image to tensor
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalize image
-])
+    model.eval()
 
-# Load and preprocess the image
-image_path = './dataset/angry/img0.png'
-image = Image.open(image_path).convert('RGB')
-input_tensor = preprocess(image)
-input_tensor = input_tensor.unsqueeze(0)  # Add batch dimension
+    # Define preprocessing transformations (same as in training)
+    preprocess = transforms.Compose([
+        transforms.Resize((48, 48)),  # Resize image to match training input size
+        transforms.ToTensor(),  # Convert image to tensor
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalize image
+    ])
 
-# Perform inference
-with torch.no_grad():  # Disable gradient computation
-    output = model(input_tensor)
+    image = Image.open(io.BytesIO(img)).convert('RGB')
+    # Load and preprocess the image
+    input_tensor = preprocess(image)
+    input_tensor = input_tensor.unsqueeze(0)  # Add batch dimension
 
-_, predicted_class = torch.max(output, 1)
-print(f'Predicted class: {predicted_class.item()}')
+    # Perform inference
+    with torch.no_grad():  # Disable gradient computation
+        output = model(input_tensor)
+
+    _, predicted_class = torch.max(output, 1)
+    return predicted_class.item()
+    print(f'Predicted class: {predicted_class.item()}')
+
+
+def generate_confusion_matrix():
+
+    labels = {
+            "happy": 0,
+            "angry": 1,
+            "engaged": 2,
+            "neutral": 3,
+        }
+    
+    model = BrenoPeterandSydneysSuperCoolConvolutionalNeuralNetworkVeryAccurateAndGood()
+    model.load_state_dict(torch.load('model.pth'))
+
+    tmp, tmp, testing_data_arr = loadData("./data.json")
+
+    confusion_matrix = [
+
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0]
+    ]
+
+    for labeled_data in testing_data_arr:
+        predicted_class = predict_emotion(model, base64.b64decode(labeled_data['img']))
+        confusion_matrix[predicted_class][labels[labeled_data['emotion']]] +=1
+
+
+    return confusion_matrix
+
+conf = generate_confusion_matrix()
+print(conf)
+
+
+
+
+
+# # Load the model
+# model = BrenoPeterandSydneysSuperCoolConvolutionalNeuralNetworkVeryAccurateAndGood()
+# model.load_state_dict(torch.load('model.pth'))
+# model.eval()
+
+# # Define preprocessing transformations (same as in training)
+# preprocess = transforms.Compose([
+#     transforms.Resize((48, 48)),  # Resize image to match training input size
+#     transforms.ToTensor(),  # Convert image to tensor
+#     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))  # Normalize image
+# ])
+
+# # Load and preprocess the image
+# image_path = './dataset/happy/img0.png'
+# image = Image.open(image_path).convert('RGB')
+# input_tensor = preprocess(image)
+# input_tensor = input_tensor.unsqueeze(0)  # Add batch dimension
+
+# # Perform inference
+# with torch.no_grad():  # Disable gradient computation
+#     output = model(input_tensor)
+
+# _, predicted_class = torch.max(output, 1)
+# print(f'Predicted class: {predicted_class.item()}')
